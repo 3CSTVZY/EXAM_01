61b35219573d1ba07f15367e149e5ca3a61d2f13
3CSTVZY <corp.of.alexeev@gmail.com>
2022-11-12 20:44:08 +0200

    2nd step - algorithm

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -3,3 +3,24 @@
 Console.WriteLine("How many massives are here? ");
 int size = Convert.ToInt32(Console.ReadLine());
 
+#region 
+string[] arrayString = new string[size];
+  for (int i=0; i<size; i++)
+{
+  Console.WriteLine($"Enter the {i+1} element: ");
+  string element = Convert.ToString(Console.ReadLine()??"");
+  arrayString[i] = element;
+}
+string[] arrayTotal = new string[size];
+int len = 3;
+int pos = 0;
+  for (int j=0; j<size; j++)
+{
+  if (arrayString[j].Length<=len)
+  {
+    arrayTotal[pos] = arrayString[j]; 
+    pos++;
+  }
+}
+#endregion
+
